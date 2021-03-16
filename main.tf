c6916a2cec060a369a242dfdbd63f6f837e60e7d
logikil <albert.davila@live.com>
2021-03-16 09:58:49 -0600

    modify security rule

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "azurerm_network_security_rule" "learning" {
   destination_port_range = "22"
   destination_address_prefix = "*"
   resource_group_name = azurerm_resource_group.learning.name
-  network_security_group_name = azurerm_
+  network_security_group_name = azurerm_network_security_group.learning.name
 }
 
 #Create vm nic and internal IP configuration
